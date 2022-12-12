95c969fc373a1d866957e2735edc6f76fbe8aad3
edmundmagnojr-nabepero <edmund.magno@nabepero.co.jp>
2022-12-12 15:55:23 +0800

    finished polymorphic many to many relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Video;
+use App\Models\Tag;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+    $post = Post::create(['name'=>'My first post']);
+    $tag1 = Tag::find(1);
+    $post->tags()->save($tag1);
+    $video = Video::create(['name'=>'video.mov']);
+    $tag2 = Tag::find(2);
+    $video->tags()->save($tag2);
+});
+
+Route::get('/read', function(){
+    $post = Post::findOrFail(1);
+    foreach($post->tags as $tag){
+        echo $tag;
+    }
+});
+
+Route::get('/update', function(){
+    // $post = Post::findOrFail(1);
+    // foreach($post->tags as $tag){
+    //     return $tag->whereName('PHP')->update(['name'=>'Updated PHP']);
+    // }
+
+    $post = Post::findOrFail(1);
+    $tag = Tag::find(3);
+    //$post->tags()->save($tag);
+    $post->tags()->attach($tag);
+    $post->tags()->sync([1,2]);
+
+});
+
+Route::get('/delete', function(){
+    $post = Post::find(1);
+    foreach($post->tags as $tag){
+        $tag->whereId(1)->delete();
+    }
 });
